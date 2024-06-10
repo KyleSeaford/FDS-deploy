3074e1d8d0fbcb80d381568428839d16c1b9b409
KyleSeaford <kyleseaford5@gmail.com>
2024-06-10 17:17:25 +0100

    update zone1 address in app.py

--- dash/app.py
+++ dash/app.py
@@ -54,7 +54,7 @@ def current_time():
 
 
 # add the addresses of the units for the different zones
-zone1_addresses_full = ['rough-snowflake-4885.ploomberapp.io']
+zone1_addresses_full = ['cool-pond-4702.ploomberapp.io']
 zone2_addresses_full = ['']
 zone3_addresses_full = ['']
 zone4_addresses_full = ['']
