17d281605c41942a25fe5ed17d44ed15611e572b
KyleSeaford <kyleseaford5@gmail.com>
2024-06-22 17:56:54 +0100

    update text display for weather conditions and windspeed

--- dash/static/js/info/conditions.js
+++ dash/static/js/info/conditions.js
@@ -1,11 +1,12 @@
 $(document).ready(function () {
     const a =  window.location.origin + '/conditions';
     $.getJSON(a, function (data) {
-        $('#conditions').text('Current Conditions: ' + data.conditions);
+        $('#conditions').text('Conditions: ' + data.conditions);
     });
     setInterval(function () {
         $.getJSON(a, function (data) {
-            $('#conditions').text('Current Conditions: ' + data.conditions);
+            $('#conditions').text('Conditions: ' + data.conditions);
         });
     }, 172800); // Update every 2.88 minutes, optimal amount of time to update due to api limit
 });
+ 
